4e24f495e76d9f90b11567c3f354241018a875e8
Vivien Galuchot <vivien.galuchot@gmail.com>
2020-08-20 11:22:43 +0200

    template pages

--- server/http.mjs
+++ server/http.mjs
@@ -5,10 +5,11 @@
 
 import { logDebug, logInfo, logError } from './log.mjs';
 
-import http from 'http' ;
-import fs from 'fs' ;
-import url from 'url' ;
-import path from 'path' ;
+import http from 'http';
+import fs from 'fs';
+import url from 'url';
+import path from 'path';
+import nunjucks from 'nunjucks';
 
 
 let mimeMap = new Map();
@@ -22,12 +23,9 @@ mimeMap.set(".svg", "image/svg+xml");
 
 
 export function initHttpServer() {
-    let port = process.env.PORT;
-    if (!port) {
-        port = "8080";
-        logInfo(`defaulting to port ${port}`);
-    }
-    
+
+    // files
+
     function sendFile(pathname, response) {
         fs.readFile(pathname, (err, data) => {
             if (err) {
@@ -40,20 +38,51 @@ export function initHttpServer() {
             let mime = mimeMap.get(ext);
             if (mime)
                 response.setHeader("Content-Type", mimeMap.get(ext));
-    
+
             response.setHeader("Cache-Control", "public,max-age=3600");
             response.writeHead(200);
             response.end(data);
         });
     }
-    
+
+    // templating
+
+    nunjucks.configure('server/templates', { autoescape: true });
+
+    function sendTemplate(template, name, request, response) {
+        response.setHeader("Cache-Control", "public,max-age=3600");
+        try {
+            let render = nunjucks.render(template, { templateName: name, request: request });
+            response.writeHead(200);
+            response.end(render);
+        } catch (error) {
+            logError(`error rendering template ${template}, ${error.message}`);
+            response.writeHead(500);
+            response.end("internal error");
+        }
+    }
+
+    let indexView = (request, response) => {
+        sendTemplate("index.html", "index", request, response);
+    }
+    let tryItView = (request, response) => {
+        sendTemplate("try-it.html", "try-it", request, response);
+    }
+
+    // server
+
+    let port = process.env.PORT;
+    if (!port) {
+        port = "8080";
+        logInfo(`defaulting to port ${port}`);
+    }
+
     const httpServer = http.createServer(function (request, response) {
         var pathname = url.parse(request.url).pathname;
-        // serve static files
-        if (pathname == "/") {
-            sendFile("client/index.html", response);
-        } else if (pathname == "/try-it") {
-            sendFile("client/try-it.html", response);
+        if (pathname == "/" || pathname == "/index" || pathname == "/index.html") {
+            indexView(request, response);
+        } else if (pathname == "/try-it" || pathname == "/try-it.html") {
+            tryItView(request, response);
         } else {
             sendFile("./client/" + pathname, response);
         }
